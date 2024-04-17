0b84b85f48472d6178a89efbe2f361d9f2cdc461
Onyx Vega <mabs.24gl@gmail.com>
2024-04-17 00:07:02 -0400

    changed port number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const api = require('./routes/index.js');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3015;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
